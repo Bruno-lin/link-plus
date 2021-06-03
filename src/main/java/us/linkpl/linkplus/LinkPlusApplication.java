33e9535dcfb28d90572f371943e195a5264d5e59
samsara98 <wk1044481101@163.com>
2021-06-03 09:23:42 +0800

    Init

--- src/main/java/us/linkpl/linkplus/LinkPlusApplication.java
+++ src/main/java/us/linkpl/linkplus/LinkPlusApplication.java
@@ -1,11 +1,9 @@
 package us.linkpl.linkplus;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan(basePackages = "us.linkpl.linkplus.mapper")
 public class LinkPlusApplication {
 
     public static void main(String[] args) {
