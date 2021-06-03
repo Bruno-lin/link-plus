f087eaa48d42672c6c2df82873c7154bb5fc718d
zzx <1176328868@qq.com>
2021-06-03 17:32:43 +0800

    camel-case

--- src/main/java/us/linkpl/linkplus/controller/AccountController.java
+++ src/main/java/us/linkpl/linkplus/controller/AccountController.java
@@ -1,9 +1,12 @@
 package us.linkpl.linkplus.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+import us.linkpl.linkplus.entity.Account;
+import us.linkpl.linkplus.mapper.AccountMapper;
 
 /**
  * <p>
@@ -16,5 +19,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/linkplus/account")
 public class AccountController {
+    @Autowired
+    AccountMapper accountMapper;
+
+    @RequestMapping("/a")
+    public Account get(){
+        return accountMapper.selectById(1);
+    }
 
 }
