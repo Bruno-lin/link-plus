a0e7d668936eaec22cd6cd5fa1dc3013db3c117b
samsara98 <wk1044481101@163.com>
2021-06-03 09:23:42 +0800

    Init

---
+++ src/main/java/us/linkpl/linkplus/LinkPlusApplication.java
@@ -0,0 +1,13 @@
+package us.linkpl.linkplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkPlusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/us/linkpl/linkplus/LinkPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package us.linkpl.linkplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
