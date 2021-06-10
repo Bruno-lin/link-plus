99a22571b0c127076e3d112918d004c22a47c5f0
samsara98 <wk1044481101@163.com>
2021-06-10 23:44:05 +0800

    Init

--- src/main/java/us/linkpl/linkplus/interceptor/AuthInterceptor.java
+++ src/main/java/us/linkpl/linkplus/interceptor/AuthInterceptor.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 
 import org.springframework.http.HttpCookie;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -20,9 +21,6 @@ public class AuthInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-
-        response.addHeader("Access-Control-Allow-Origin","http://localhost:8080");
-        response.addHeader("Access-Control-Allow-Credentials","true");
 
         HttpSession session = request.getSession();
         Cookie[] cookies = request.getCookies();
@@ -44,5 +42,11 @@ public class AuthInterceptor implements HandlerInterceptor {
         }
         response.setStatus(401);
         return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,  ModelAndView modelAndView) throws Exception {
+        response.addHeader("Access-Control-Allow-Origin", "http://localhost:8082");
+        response.addHeader("Access-Control-Allow-Credentials", "true");
     }
 }
